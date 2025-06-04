834f8f4b27d8d831902fb547b05b5aff503a4564
Guilherme Lincoln <153618107+glincolndev@users.noreply.github.com>
2025-06-04 15:26:30 -0300

    Update creditos.js

--- creditos.js
+++ creditos.js
@@ -1,9 +1,17 @@
-creditos(){
-
+function creditos() {
   background(img);
-  
-  text("feito por:
-       Guilherme Lincoln
-       Artur Alguma Coisa (foi mal)")
-
+  textSize(16);
+  fill(0);
+  textAlign(LEFT);
+  text(
+    'Feito por:\n' +
+    'Guilherme Lincoln de Oliveira Lima\n' +
+    'guilherme.oliveira.107@ufrn.edu.br\n\n' +
+    'Artur Daniel Borges Lopes\n' +
+    'artur.lopes.701@ufrn.edu.br\n\n' +
+    'Imagens utilizadas:\n' +
+    'https://drive.google.com/file/d/\n14ScxooBvmuRJNOnQn3Ehk0_XuWRgn4wW/\nview?usp=sharing',
+    100, 179
+  );
+   
 }
